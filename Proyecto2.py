a05a2827d4a906bd340f834aadab25642ee28c0f
reddubois <54864123+reddubois@users.noreply.github.com>
2022-09-19 16:10:43 -0600

    Update Proyecto2.py

--- Proyecto2.py
+++ Proyecto2.py
@@ -71,7 +71,28 @@ plt.title('Cantidad de conceptos por artículo')
 # con frecuencias de 511, 156, 133, 130, 116.
 # len(df2['mention'].value_counts())
 
+
+# Largo de las menciones
+# Media de 10 caracteres, std de 8 caracteres.
+# Min 1 caracter, max de 110.
+# El 80% de las menciones tienen menos de 13 caracteres.
+# Ejemplos de min son abreviaturas. Un abstract puede contener una mencion muy recurrente que se decide
+# abreviar a una sola letra, como Testosterona (T).
+# Deciles array([ 1.,  3.,  4.,  5.,  5.,  7.,  8., 10., 13., 20.])
+
+# df1.loc[df1['abstract_id']==15064320]['abstract'].values
+# np.percentile(df2['offset_finish'] - df2['offset_start'], np.arange(0, 100, 10))
+plt.clf()
+plt.hist(df2['offset_finish'] - df2['offset_start'], bins=13, range=(1,14))
+plt.xlabel('Longitud')
+plt.ylabel('Frecuencia')
+plt.title('Longitud de mención')
+#plt.show()
+
+
 # Hay 6 tipos de concepto. Las frecuencias de estas son:
+# Mayor concepto GeneOrGeneProduct con 4764.
+# Menor concepto CellLine con 117. 
 print(df2['type'].value_counts())
 plt.clf()
 df2['type'].value_counts().sort_values(ascending=True).plot(kind='barh')
@@ -79,5 +100,20 @@ plt.xlabel('Frecuencia')
 plt.title('Menciones por tipo de concepto')
 #plt.show()
 
+
+# Posicion de las menciones dentro de los abstract, en medida porcentual.
+# Distribucion casi uniforme, con una ligera concentracion de
+# menciones al inicio de los articulos (posiblemente por la presencia de los titulos)
+dfLen = pd.DataFrame({'abstract_id':df1['abstract_id'],
+             'lenTotal': lenTitle + lenAbstract})
+
+df2_Lens = pd.merge(dfLen, df2, how='right', on=['abstract_id'])
+offset_start_percent = df2_Lens['offset_start']/df2_Lens['lenTotal']
+plt.clf()
+plt.hist(offset_start_percent)
+plt.title('Posición de mención respecto longitud total')
+plt.xlabel('offset_start / lenTotal')
+plt.ylabel('Frecuencia')
+#plt.show()
 
 
